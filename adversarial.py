631cd7af69d8102c4cd72fec95c68635704974e1
ivan <ivanzhangsolutions@gmail.com>
2017-09-30 17:56:19 -0400

    try wiggling

--- adversarial.py
+++ adversarial.py
@@ -132,10 +132,41 @@ def main(_):
         training: False
     })
 
-    X_adv = batch[0][:]
-    X_adv = X_adv + np.sign(gradients_og) * FLAGS.eps
-    X_adv = X_adv - np.sign(gradients_adv) * FLAGS.eps
+    # wiggle pixels for each image
+    X_adv = np.array(batch[0][:])
+    X_adv = X_adv + np.sign(gradients_og) * FLAGS.eps / 2
+    X_adv = X_adv - np.sign(gradients_adv) * FLAGS.eps * 2
     X_adv = np.clip(X_adv, 0.0, 1.0)
+    classification = sess.run([prediction],
+                              {x: X_adv,
+                               y_: batch[1],
+                               training: False})
+
+    # wiggling
+    for i in range(len(X_adv)):
+      count = 0
+      while classification[0][i] != FLAGS.target and count < 50:
+        X_adv[i] = X_adv[i] + np.sign(gradients_og[i]) * FLAGS.eps * 0.001
+        X_adv[i] = X_adv[i] - np.sign(gradients_adv[i]) * FLAGS.eps * 0.001
+        X_adv[i] = np.clip(X_adv[i], 0.0, 1.0)
+        classification = sess.run([prediction],
+                                  {x: X_adv,
+                                   y_: batch[1],
+                                   training: False})
+        gradients_adv = grd.eval({
+            x: X_adv,
+            y_: target_batch[1],
+            training: False
+        })
+
+        #gradients_og = grd.eval({x: X_adv, y_: batch[1], training: False})
+
+        percent = y.eval({x: X_adv, y_: batch[1], training: False})
+        print(i, percent[i][FLAGS.target], percent[i][classification[0][i]])
+        count += 1
+        if count == 50:
+          print("exit due to limit")
+
     classification = sess.run([prediction],
                               {x: X_adv,
                                y_: batch[1],
